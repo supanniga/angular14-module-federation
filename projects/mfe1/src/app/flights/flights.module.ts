ab250b6a87bc54bc565d5e330bf776aba82a1255
supanniga <supanniga.oga@gec.co.th>
2022-11-01 16:31:21 +0700

    [mfe1] ng g c lazy --module flights

--- projects/mfe1/src/app/flights/flights.module.ts
+++ projects/mfe1/src/app/flights/flights.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FlightsRoutingModule } from './flights-routing.module';
+import { LazyComponent } from './lazy/lazy.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LazyComponent,
+  ],
   imports: [
     CommonModule,
     FlightsRoutingModule
