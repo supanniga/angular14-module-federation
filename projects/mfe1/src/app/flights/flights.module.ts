abf459d97f391e010e11d49151e402e794a86fdc
supanniga <supanniga.oga@gec.co.th>
2022-11-01 14:53:18 +0700

    ng g m flights --routing true in mfe1 app

---
+++ projects/mfe1/src/app/flights/flights.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FlightsRoutingModule } from './flights-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FlightsRoutingModule
+  ]
+})
+export class FlightsModule { }
