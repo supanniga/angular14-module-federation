15bee60a95b66893a5ad3c3e784d23a374c0d702
supanniga <supanniga.oga@gec.co.th>
2022-11-01 16:31:52 +0700

    [mfe1] ng g c flights-search --module flights

---
+++ projects/mfe1/src/app/flights/flights-search/flights-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flights-search',
+  templateUrl: './flights-search.component.html',
+  styleUrls: ['./flights-search.component.css']
+})
+export class FlightsSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/mfe1/src/app/flights/flights.module.ts
+++ projects/mfe1/src/app/flights/flights.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { FlightsRoutingModule } from './flights-routing.module';
 import { LazyComponent } from './lazy/lazy.component';
+import { FlightsSearchComponent } from './flights-search/flights-search.component';
 
 
 @NgModule({
   declarations: [
     LazyComponent,
+    FlightsSearchComponent
   ],
   imports: [
     CommonModule,
