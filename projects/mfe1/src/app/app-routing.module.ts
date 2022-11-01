c0240fa6559bc947db75aa13bbc64c38c8a6ecce
supanniga <supanniga.oga@gec.co.th>
2022-11-01 17:15:17 +0700

    [mfe1] add lazy flights route

--- projects/mfe1/src/app/app-routing.module.ts
+++ projects/mfe1/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { FlightsSearchComponent } from './flights/flights-search/flights-search.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
@@ -7,6 +8,10 @@ const routes: Routes = [
     path: '',
     component: HomeComponent,
     pathMatch: 'full'
+  },
+  {
+    path: 'flights',
+    loadChildren: () => import('./flights/flights.module').then(m => m.FlightsModule)
   }
 ];
 
