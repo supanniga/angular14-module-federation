04157d41ecce165a48d4658a4e2e0ba5525fbd65
supanniga <supanniga.oga@gec.co.th>
2022-11-01 16:45:33 +0700

    [shell] add not-found route

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   },
 ];
 
