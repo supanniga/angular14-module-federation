a8f94172b329b391f87a55e28a3ef21e12a9c455
supanniga <supanniga.oga@gec.co.th>
2022-11-01 14:36:10 +0700

    ng g c not-found in shell app

--- projects/shell/src/app/app.module.ts
+++ projects/shell/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
