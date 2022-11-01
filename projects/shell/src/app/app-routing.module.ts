17df38e9ff9b3c47e62582be6125412dd3116617
supanniga <supanniga.oga@gec.co.th>
2022-11-01 17:22:29 +0700

    [shell] พื้นฐานการใช้ Dynamic Federation เหมือน lazy load ใน angular ต้องใส่ type: 'module' ตั้งแต่ angular 13 ขึ้นไปเพราะ the CLI emits EcmaScript modules instead of "plain old" JavaScript files.

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
@@ -11,8 +12,14 @@ const routes: Routes = [
   },
   {
     path: 'flights',
-    loadChildren: () => import('mfe1/Module').then(m => m.FlightsModule)
-  },
+    loadChildren: () =>
+         loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4201/remoteEntry.js',
+            exposedModule: './Module'
+        })
+        .then(m => m.FlightsModule)
+},
   {
     path: '**',
     component: NotFoundComponent
