6a357a4253234ca7ed4846b030b5d4b3ec3f25a7
supanniga <supanniga.oga@gec.co.th>
2022-11-01 17:15:56 +0700

    [mfe1] add flight-search route

--- projects/mfe1/src/app/flights/flights-routing.module.ts
+++ projects/mfe1/src/app/flights/flights-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FlightsSearchComponent } from './flights-search/flights-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'flights-search',
+    component: FlightsSearchComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
